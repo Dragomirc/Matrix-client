6c518ac250d6e2a2295b26480070db7be7484199
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2019-08-23 16:39:56 +0100

    added style scripts

---
+++ scripts/sass.js
@@ -0,0 +1,18 @@
+const { sh } = require('tasksfile')
+const fs = require('fs');
+const path = require('path');
+
+const APP_FOLDER = path.resolve(__dirname, '../app/');
+
+if ( fs.existsSync(APP_FOLDER + '/src/styles/style.scss') ) {
+    if ( !fs.existsSync(APP_FOLDER + '/compiled') ) {
+        fs.mkdirSync(APP_FOLDER + '/compiled');
+    }
+    if ( !fs.existsSync(APP_FOLDER + '/compiled/styles') ) {
+        fs.mkdirSync(APP_FOLDER + '/compiled/styles');
+    }
+    
+    sh('node ./node_modules/sass/sass.js app/src/styles/style.scss app/compiled/styles/vendor.css');
+} else {
+    console.log('No sass file found');
+}
