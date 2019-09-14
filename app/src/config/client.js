8fff0a20615b27aa5faf465e9b8328bab93e89ab
Dragomir <svedava@mail.ru>
2019-09-14 23:52:37 +0100

    updated the config file

--- app/src/config/client.js
+++ app/src/config/client.js
@@ -2,7 +2,7 @@ class ClientConfig {
     static fetch() {
         let services = {
             proxy: "http://localhost:8080",
-            xForwardedHost: "'localhost:3000'",
+            xForwardedHost: "",
             shop: "http://localhost:3000/api/shop",
             admin: "http://localhost:3000/api/admin",
             auth: "http://localhost:3000/api/auth"
@@ -10,11 +10,11 @@ class ClientConfig {
 
         if (process.env.NODE_ENV === "production") {
             services = {
-                proxy: "https://git.heroku.com/matrix-rest.git",
-                xForwardedHost: "",
-                shop: "https://git.heroku.com/matrix-client.git/api/shop",
-                admin: "https://git.heroku.com/matrix-client.git/api/admin",
-                auth: "https://git.heroku.com/matrix-client.git/api/auth"
+                proxy: "https://matrix-rest.herokuapp.com",
+                xForwardedHost: "matrix-client.herokuapp.com",
+                shop: "https://matrix-client.herokuapp.com/api/shop",
+                admin: "https://matrix-client.herokuapp.com/api/admin",
+                auth: "https://matrix-client.herokuapp.com/api/auth"
             };
         }
         return new Promise(resolve => {
