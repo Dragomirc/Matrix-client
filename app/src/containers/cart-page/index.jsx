90ecba3e1304bd2a259eadabfc35c8206bcb1446
Dragomir Ceban <dragomir.ceban@reedonline.co.uk>
2019-10-03 17:47:49 +0100

    converted the cart-page ocntianerto a class component

--- app/src/containers/cart-page/index.jsx
+++ app/src/containers/cart-page/index.jsx
@@ -1,63 +1,83 @@
 /* eslint-disable react/jsx-curly-newline */
-import React from "react";
-import { Container, Col, Button, ListGroup, ListGroupItem } from "reactstrap";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import classnames from "classnames";
-import { deleteCartItem } from "app/redux/actions/user";
-import OrderDetails from "app/components/order-details";
-import requireAuth from "app/hoc/require-auth";
-import styles from "./styles.scss";
+/* eslint-disable */
+import React, { Component } from 'react';
+import { Container, Col, Button, ListGroup, ListGroupItem } from 'reactstrap';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { deleteCartItem } from 'app/redux/actions/user';
+import OrderDetails from 'app/components/order-details';
+import requireAuth from 'app/hoc/require-auth';
+import styles from './styles.scss';
 
-const CartPage = ({ cart, deleteCartItemConnect }) => {
-    const isCartEmpty = cart.length === 0;
-    let cartHeadingText = "Your Cart";
-    if (isCartEmpty) {
-        cartHeadingText = "Your Cart is Empty!";
+class CartPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showOrderDetailsForm: false
+        };
     }
-    let orderButtonView = null;
-    if (!isCartEmpty) {
-        orderButtonView = (
-            <Button
-                className={classnames("mx-auto my-3 d-block", styles.button)}
-            >
-                Checkout
-            </Button>
+    render() {
+        const { cart, deleteCartItemConnect } = this.props;
+        const isCartEmpty = cart.length === 0;
+        let total = 0;
+        const cartItemsView = cart.map(({ _id, productId, quantity }) => {
+            total += productId.price;
+            return (
+                <ListGroupItem className="d-flex align-items-center" key={_id}>
+                    <Col>{productId.title}</Col>
+                    <Col>{`Quantitiy x ${quantity}`}</Col>
+                    <Col className="flex-grow-0">
+                        <Button
+                            className={styles.button}
+                            onClick={() => deleteCartItemConnect(productId._id)}
+                        >
+                            Delete
+                        </Button>
+                    </Col>
+                </ListGroupItem>
+            );
+        });
+        let cartHeadingText = 'Your Cart';
+        if (isCartEmpty) {
+            cartHeadingText = 'Your Cart is Empty!';
+        }
+        let orderButtonView = null;
+
+        let totalView = null;
+        if (!isCartEmpty) {
+            orderButtonView = (
+                <Button
+                    className={classnames(
+                        'mx-auto my-3 d-block',
+                        styles.button
+                    )}
+                >
+                    Place Order
+                </Button>
+            );
+            totalView = (
+                <Col className="mt-3 text-center font-weight-bold">{`Total ${total}$`}</Col>
+            );
+        }
+
+        return (
+            <Container>
+                <ListGroup className="w-80 mx-auto mt-3">
+                    <ListGroupItem>
+                        <Col className="text-center">{cartHeadingText}</Col>
+                    </ListGroupItem>
+                    {cartItemsView}
+                </ListGroup>
+
+                {totalView}
+                {orderButtonView}
+                <Col className="w-80 mx-auto border-bottom mt-4" />
+                <OrderDetails />
+            </Container>
         );
     }
-    return (
-        <Container>
-            <ListGroup className="w-80 mx-auto mt-3">
-                <ListGroupItem>
-                    <Col className="text-center">{cartHeadingText}</Col>
-                </ListGroupItem>
-                {cart.map(({ _id, productId, quantity }) => (
-                    <ListGroupItem
-                        className="d-flex align-items-center"
-                        key={_id}
-                    >
-                        <Col>{productId.title}</Col>
-                        <Col>{`Quantitiy x ${quantity}`}</Col>
-                        <Col className="flex-grow-0">
-                            <Button
-                                className={styles.button}
-                                onClick={() =>
-                                    deleteCartItemConnect(productId._id)
-                                }
-                            >
-                                Delete
-                            </Button>
-                        </Col>
-                    </ListGroupItem>
-                ))}
-            </ListGroup>
-
-            {orderButtonView}
-            <Col className="w-80 mx-auto border-bottom" />
-            <OrderDetails />
-        </Container>
-    );
-};
+}
 
 const mapStateToProps = ({ user }) => ({ cart: user.cart });
 const mapDispatchToProps = { deleteCartItemConnect: deleteCartItem };
